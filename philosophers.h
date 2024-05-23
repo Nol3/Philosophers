e12c7d60bfc0dbad47656a4e3a2dc5c0caf152ee
Alejandro Cardenas Parejo <alcarden@c1r2s2.42malaga.com>
2024-05-23 18:36:23 +0200

    primer_dia_linux

---
+++ philosophers.h
@@ -0,0 +1,16 @@
+#ifdef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <errno.h>
+# include <string.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <limits.h>
+
+#endif
