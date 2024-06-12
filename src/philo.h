13d9d964b097cd5b8505bea15ed3469a093eefdd
Alejandro Cardenas Parejo <alcarden@c3r7s6.42malaga.com>
2024-06-12 14:47:26 +0200

    hora de comer

--- src/philo.h
+++ src/philo.h
@@ -6,25 +6,25 @@
 /*   By: alcarden <alcarden@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 23:58:10 by alcarden          #+#    #+#             */
-/*   Updated: 2024/06/11 18:28:12 by alcarden         ###   ########.fr       */
+/*   Updated: 2024/06/12 14:47:00 by alcarden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <errno.h>
+# include <inttypes.h>
+# include <limits.h>
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdint.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <stdint.h>
+# include <string.h>
+# include <sys/time.h>
 # include <sys/wait.h>
 # include <unistd.h>
-# include <stdbool.h>
-# include <errno.h>
-# include <string.h>
-# include <pthread.h>
-# include <sys/time.h>
-# include <limits.h>
-# include <inttypes.h>
 
 typedef pthread_mutex_t	t_mtx;
 
@@ -33,7 +33,7 @@ typedef enum e_time_code
 	SECONDS,
 	MILLISECOND,
 	MICROSECOND,
-}		t_time_code;
+}						t_time_code;
 
 typedef enum e_status
 {
@@ -43,7 +43,7 @@ typedef enum e_status
 	TAKE_FIRST_FORK,
 	TAKE_SECOND_FORK,
 	DIED,
-}			t_philo_status;
+}						t_philo_status;
 
 typedef enum e_opcode
 {
@@ -54,71 +54,73 @@ typedef enum e_opcode
 	CREATE,
 	JOIN,
 	DETACH,
-}			t_opcode;
+}						t_opcode;
 
 typedef struct s_philo
 {
-	int				id;
-	int				nb_meals_had;
-	struct s_data	*data;
-	t_state			state;
-	pthread_mutex_t	*left_f;
-	pthread_mutex_t	*right_f;
-	pthread_mutex_t	mut_state;
-	pthread_mutex_t	mut_nb_meals_had;
-	pthread_mutex_t	mut_last_eat_time;
-	u_int64_t		last_eat_time;
-}				t_philo;
-
+	int					id;
+	int					nb_meals_had;
+	struct s_data		*data;
+	t_state				state;
+	pthread_mutex_t		*left_f;
+	pthread_mutex_t		*right_f;
+	pthread_mutex_t		mut_state;
+	pthread_mutex_t		mut_nb_meals_had;
+	pthread_mutex_t		mut_last_eat_time;
+	u_int64_t			last_eat_time;
+}						t_philo;
 
 typedef struct s_data
 {
-	int				nb_philos;
-	bool			keep_iterating;
-	u_int64_t		die_time;
-	u_int64_t		eat_time;
-	u_int64_t		sleep_time;
-	int				nb_meals;
-	
-	int				nb_full_p;
-	u_int64_t		start_time;
-	pthread_mutex_t	mut_eat_t;
-	pthread_mutex_t	mut_die_t;
-	pthread_mutex_t	mut_sleep_t;
-	pthread_mutex_t	mut_print;
-	pthread_mutex_t	mut_nb_philos;
-	pthread_mutex_t	mut_keep_iter;
-	pthread_mutex_t	mut_start_time;
-	pthread_t		check_all_alive;
-	pthread_t		check_all_full;
-	pthread_t		*philo_ths;
-	pthread_mutex_t	*forks;
-	t_philo			*philos;
-}				t_data;
+	int					nb_philos;
+	bool				keep_iterating;
+	u_int64_t			die_time;
+	u_int64_t			eat_time;
+	u_int64_t			sleep_time;
+	int					nb_meals;
 
-//Prototypes
+	int					nb_full_p;
+	u_int64_t			start_time;
+	pthread_mutex_t		mut_eat_t;
+	pthread_mutex_t		mut_die_t;
+	pthread_mutex_t		mut_sleep_t;
+	pthread_mutex_t		mut_print;
+	pthread_mutex_t		mut_nb_philos;
+	pthread_mutex_t		mut_keep_iter;
+	pthread_mutex_t		mut_start_time;
+	pthread_t			check_all_alive;
+	pthread_t			check_all_full;
+	pthread_t			*philo_ths;
+	pthread_mutex_t		*forks;
+	t_philo				*philos;
+}						t_data;
 
-//main.c
-void		ft_error_exit(const char *error);
+// Prototypes
 
-//parse.c
-int					ft_isdigit(int c);
-static bool			ft_isspace(char c);
-static const char	*ft_valid_args(const char *str);
-static long			ft_atol(const char *str);
-void				ft_parse_args(t_data *t_data, int argc, char *argv[]);
+// utils.c
+void					ft_error_exit(const char *error);
+long					ft_start_time(void);
+void					ft_usleep(long time);
+long					ft_current_time(long start_time);
 
-//init.c
-void				ft_init_data(t_data *data);
-t_philo*			init_philo(int id, t_data* data);
-t_data*				init_data(int nb_philos);
+// parse.c
+int						ft_isdigit(int c);
+static bool				ft_isspace(char c);
+static const char		*ft_valid_args(const char *str);
+static long				ft_atol(const char *str);
+void					ft_parse_args(t_data *t_data, int argc, char *argv[]);
 
-//safe_inits.c
-void				ft_safe_malloc(size_t bytes);
-void				ft_safe_mutex_handle(t_mtx *mutex, t_opcode opcode);
-static void			ft_handle_thread_error(int status, t_opcode opcode);
-void 				ft_safe_mutex_handle(t_mtx *mutex, t_opcode opcode);
-void				ft_safe_thread_handle(pthread_t *thread, 
-										void *(*foo)(void *));
+// init.c
+void					ft_init_data(t_data *data);
+t_philo					*init_philo(int id, t_data *data);
+t_data					*init_data(int nb_philos);
+
+// safe_inits.c
+void					ft_safe_malloc(size_t bytes);
+void					ft_safe_mutex_handle(t_mtx *mutex, t_opcode opcode);
+static void				ft_handle_thread_error(int status, t_opcode opcode);
+void					ft_safe_mutex_handle(t_mtx *mutex, t_opcode opcode);
+void					ft_safe_thread_handle(pthread_t *thread,
+							void *(*foo)(void *));
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: alcarden <alcarden@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 13:29:24 by alcarden          #+#    #+#             */
-/*   Updated: 2024/06/11 11:22:54 by alcarden         ###   ########.fr       */
+/*   Updated: 2024/06/12 14:46:44 by alcarden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,33 @@ void	ft_error_exit(const char *error)
 {
 	printf("%s\n", error);
 	exit(EXIT_FAILURE);
+}
+
+long	ft_start_time(void)
+{
+	struct timeval	time;
+	long			start_time;
+
+	gettimeofday(&time, NULL);
+	start_time = time.tv_sec * 1000 + time.tv_usec / 1000;
+	return (start_time);
+}
+
+void	ft_usleep(long time)
+{
+	long	start_time;
+
+	start_time = ft_start_time();
+	while (ft_start_time() - start_time < time)
+		usleep(100);
+}
+
+long	ft_current_time(long start_time)
+{
+	struct timeval	time;
+	long			current_time;
+
+	gettimeofday(&time, NULL);
+	current_time = time.tv_sec * 1000 + time.tv_usec / 1000;
+	return (current_time - start_time);
 }
